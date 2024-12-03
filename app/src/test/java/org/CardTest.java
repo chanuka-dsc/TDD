fe1c3c5ea22800074a0dfa787c7ade5ae8738488
chanuka-dsc <85240799+chanuka-dsc@users.noreply.github.com>
2024-12-03 20:04:10 +0100

    Test for checking  card show behaviour added

--- app/src/test/java/org/CardTest.java
+++ app/src/test/java/org/CardTest.java
@@ -1,5 +1,25 @@
 package org;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class CardTest {
 
+  @Test
+  void testShowCard() {
+
+    Value value = Value.QUEEN;
+    Suite suite = Suite.HEARTS;
+
+    Card card = new Card(value, suite);
+
+    assertEquals(card.isHidden(), true);
+    assertEquals(card.toString(), "Hidden");
+
+    card.show();
+    assertEquals(card.isHidden(), false);
+    assertEquals(card.toString(), "Queen of Hearts");
+
+  }
 }
