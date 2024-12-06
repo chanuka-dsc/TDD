693493c4dabb856fc57fb3050231c7c107d86684
Dhruv <dg222jz@student.lnu.se>
2024-12-06 12:19:59 +0100

    get hand stub updated

--- app/src/main/java/org/components/Player.java
+++ app/src/main/java/org/components/Player.java
@@ -1,6 +1,12 @@
 package org.components;
 
+import java.util.ArrayList;
+
 public class Player {
-    
+
+    public ArrayList<Card> getHand() {
+        return new ArrayList<Card>();
+    }
+
     
 }
